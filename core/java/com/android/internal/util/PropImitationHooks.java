c51046386a1461136a381db9d47b1d5275db9a83
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    Initial implementation of KeyboxImitationHooks
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -281,6 +281,12 @@ public class PropImitationHooks {
     }
 
     public static void onEngineGetCertificateChain() {
+        // If a keybox is found, don't block key attestation
+        if (KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
+            return;
+        }
+
         // Check stack for SafetyNet or Play Integrity
         if (isCallerSafetyNet() || sIsFinsky) {
             dlog("Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
